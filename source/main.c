85bd8c90e9dea2bc56f259f253d1009bfa5d98cf
fab9511 <fabian.nacze@gmail.com>
2026-03-07 23:16:32 +0100

    poprawa funkcji liczącej cos

--- source/main.c
+++ source/main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "my_lib.h"
 
 int main() {
@@ -9,7 +8,7 @@ int main() {
 	printf("\n%f", cos_taylor(4,10));
 	printf("\n%lf", cos_taylor_reverse(0.38945076923845889371733619555016,15));
 	printf("\n%lf", cos_recursive(0.38945076923845889371733619555016,15));
-	printf("\n%lf", cos_recursive_reverse(0.38945076923845889371733619555016,15));
+	printf("\n%.23ef", cos_recursive_reverse(0.38945076923845889371733619555016,15));
 
 	return 0;
 }
--- source/my_lib/cos.c
+++ source/my_lib/cos.c
@@ -1,4 +1,7 @@
-#include <cos.h>
+#include <stdlib.h>
+#include "cos.h"
+#include "power.h"
+#include "factorial.h"
 
 double cos_taylor(double x, int precision) {
 	double result = 0.0;
@@ -39,15 +42,14 @@ double cos_recursive_reverse(double x, int precision) {
 	double *terms = malloc(sizeof(double)*precision);
 	terms[0] = 1.0;
 
-    for (int i = 0; i < precision; i++) {
-	    terms[i] = terms[i-1] -x * x / ((2*i + 1) * (2*i + 2));
+    for (int i = 1; i < precision; i++) {
+	    terms[i] = terms[i-1] * (-x * x) / ((2*i - 1) * (2*i));
 	}
 
-    for(int i = precision-1; i >= 0; i--) {
+    for (int i = precision - 1; i >= 0; i--) {
         result += terms[i];
     }
-
+	
 	free(terms);    
 	return result;
 }
-
