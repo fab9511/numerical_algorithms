e21c91cab4f8355887291cda99c13dcf3a0e7947
fab9511 <fabian.nacze@gmail.com>
2026-03-08 21:59:04 +0100

    Dodanie testów do funkcji cos

--- source/main.c
+++ source/main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include "my_lib.h"
+#include <math.h> 
+
+#define N 1000000
 
 int main() {
-	printf("2^5: %f", power(2,10));
-	printf("\nsilnia: %d", factorial(5));
-	printf("\n%lf", cos_taylor(0.38945076923845889371733619555016,15));
-	printf("\n%f", cos_taylor(4,10));
-	printf("\n%lf", cos_taylor_reverse(0.38945076923845889371733619555016,15));
-	printf("\n%lf", cos_recursive(0.38945076923845889371733619555016,15));
-	printf("\n%.23ef", cos_recursive_reverse(0.38945076923845889371733619555016,15));
+	// zakres testu [-20Pi, 20Pi]
+	double min = -20 * M_PI;
+	double max =  20 * M_PI;
+	double step = (max - min) / N;
+	int precision = 10;
 
+   FILE *file = fopen("cos_data.csv","w");
+    fprintf(file,"x,cos_lib,V1,V2,V3,V4\n");
+
+	for (int i = 0; i < N; i++) {
+    	double x = min + i * step;
+		
+		double cos_lib = cos(x);
+        double v1 = cos_taylor(x, precision);
+        double v2 = cos_taylor_reverse(x, precision);
+        double v3 = cos_recursive(x, precision);
+        double v4 = cos_recursive_reverse(x, precision);	
+
+		fprintf(file,"%lf,%lf,%lf,%lf,%lf,%lf\n", x, cos_lib, v1, v2, v3, v4);	
+	}
+
+	fclose(file);
 	return 0;
 }
 
