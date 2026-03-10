89310cf639f33aca34af474146949ff4e278929c
fab9511 <fabian.nacze@gmail.com>
2026-03-10 11:27:16 +0100

    Dodanie w pliku main.c zapisu testów funkcji cos do pliku dane_cos.csv

--- source/main.c
+++ source/main.c
@@ -11,8 +11,13 @@ int main() {
 	double step = (max - min) / N;
 	int precision = 10;
 
-   FILE *file = fopen("cos_data.csv","w");
+	FILE *file = fopen("cos_data.csv","w");
     fprintf(file,"x,cos_lib,V1,V2,V3,V4\n");
+	
+	if (!file) {
+    	perror("fopen");
+    	return 1;
+	}
 
 	for (int i = 0; i < N; i++) {
     	double x = min + i * step;
@@ -23,7 +28,7 @@ int main() {
         double v3 = cos_recursive(x, precision);
         double v4 = cos_recursive_reverse(x, precision);	
 
-		fprintf(file,"%lf,%lf,%lf,%lf,%lf,%lf\n", x, cos_lib, v1, v2, v3, v4);	
+		fprintf(file,"%.17g,%.17g,%.17g,%.17g,%.17g,%.17g\n", x, cos_lib, v1, v2, v3, v4);	
 	}
 
 	fclose(file);
