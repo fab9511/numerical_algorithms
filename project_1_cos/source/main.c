033bc9c9c8497bce98f63b36e7ab7e5bc0c45f2c
fab9511 <fabian.nacze@gmail.com>
2026-03-13 10:14:12 +0100

    Dodanie testów sprawdzających błędy obliczeń cos w zależności od precyzji

--- project_1_cos/source/main.c
+++ project_1_cos/source/main.c
@@ -18,7 +18,7 @@ int main() {
     	perror("fopen");
     	return 1;
 	}
-
+	// testy 4 funkcji cos
 	for (int i = 0; i < N; i++) {
     	double x = min + i * step;
 		
@@ -32,7 +32,29 @@ int main() {
 	}
 
 	fclose(file);
+	
+	FILE *file_2 = fopen("cos_data_2.csv","w");
+
+	if (!file_2) {
+		perror("fopen drugi plik");
+		return 1;
+	}
+
+    fprintf(file_2,"precision,x,cos_lib,V1,V2,V3,V4\n");
+
+    double x = M_PI / 2;
+	int max_precision = 30;
+	double cos_x = cos(x);
+	//testy precyzji dla danego x	
+	for (int i = 1; i < max_precision; i++) {
+		double v1 = cos_taylor(x, i);
+        double v2 = cos_taylor_reverse(x, i);
+        double v3 = cos_recursive(x, i);
+        double v4 = cos_recursive_reverse(x, i);
+
+        fprintf(file_2,"%d,%.17g,%.17g,%.17g,%.17g,%.17g,%.17g\n", i, x, cos_x, v1, v2, v3, v4);
+    }
+	
+	fclose(file_2);
 	return 0;
 }
-
-
